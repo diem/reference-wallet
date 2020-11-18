c03a521136d17eda8dd351fd15ba18c48b953d47
Maya Marton <maya.marton@firstdag.com>
2020-11-18 14:27:07 +0200

    Buy funds to insure internal transaction (#588)

--- backend/tests/wallet_tests/resources/seeds/add_funds_seeder.py
+++ backend/tests/wallet_tests/resources/seeds/add_funds_seeder.py
@@ -8,6 +8,7 @@ from datetime import datetime, timedelta
 from typing import Tuple
 from uuid import UUID
 
+from libra_utils.types.currencies import LibraCurrency, FiatCurrency
 from tests.wallet_tests.resources.seeds import prototypes
 from wallet.services import INVENTORY_ACCOUNT_NAME
 from wallet.storage import Account, Transaction, Order
@@ -20,7 +21,6 @@ from wallet.types import (
     OrderType,
     OrderId,
 )
-from libra_utils.types.currencies import LibraCurrency, FiatCurrency
 
 
 class AddFundsSeeder:
@@ -49,6 +49,41 @@ class AddFundsSeeder:
             destination_id=inventory_account.id,
         )
         inventory_account.received_transactions.append(inventory_income)
+
+        order = Order(
+            amount=buy_amount,
+            direction=Direction.Buy,
+            base_currency=buy_currency,
+            quote_currency=pay_currency,
+            order_status=OrderStatus.PendingExecution,
+            cover_status=CoverStatus.PendingCover,
+            exchange_amount=pay_price,
+            order_expiration=datetime.utcnow() + timedelta(minutes=10),
+            order_type=OrderType.Trade,
+        )
+
+        user.orders.append(order)
+        db_session.add(order)
+        db_session.commit()
+
+        return inventory_account.id, user.account.id, OrderId(UUID(order.id))
+
+
+class InventoryWithoutFundsSeeder:
+    @staticmethod
+    def run(
+        db_session,
+        buy_amount: int,
+        buy_currency: LibraCurrency,
+        pay_currency: FiatCurrency,
+        pay_price: int,
+    ) -> Tuple[int, int, OrderId]:
+        user = deepcopy(prototypes.user)
+        user.account = Account(name="fake_account_seed")
+        inventory_account = Account(name=INVENTORY_ACCOUNT_NAME)
+        db_session.add(user)
+        db_session.add(inventory_account)
+        db_session.commit()
 
         order = Order(
             amount=buy_amount,
