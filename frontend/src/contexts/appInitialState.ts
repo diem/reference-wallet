6c480286a3d7081f3f581af950252fa8994f4763
maya <maya.marton@firstdag.com>
2020-12-16 14:46:46 +0200

    fix initial network value

--- frontend/src/contexts/appInitialState.ts
+++ frontend/src/contexts/appInitialState.ts
@@ -1,76 +1,62 @@
-import BackendClient from "../services/backendClient";
+import {AppSettings} from "../interfaces/settings";
 
-const promise = new Promise(function (resolve, reject) {
-    resolve(chainDisplayName());
-});
-
-const chainDisplayName = async (): Promise<string> => {
-    const backendClient = new BackendClient();
-
-    const chain = await backendClient.getChain();
-
-    return chain.display_name;
-};
-
-export const initialState = async () => {
-    return  {
-        network: await promise,
-        currencies: {
-            Coin1: {
-                name: "Coin1",
-                symbol: "Coin1",
-                sign: "≋Coin1",
-                rates: {
-                    USD: 1,
-                    EUR: 0.85,
-                    GBP: 0.8,
-                    CHF: 1,
-                    CAD: 1,
-                    AUD: 1,
-                    NZD: 1,
-                    JPY: 1,
-                },
+export const initialState: AppSettings = {
+    network: "",
+    currencies: {
+        Coin1: {
+            name: "Coin1",
+            symbol: "Coin1",
+            sign: "≋Coin1",
+            rates: {
+                USD: 1,
+                EUR: 0.85,
+                GBP: 0.8,
+                CHF: 1,
+                CAD: 1,
+                AUD: 1,
+                NZD: 1,
+                JPY: 1,
             },
         },
-        fiatCurrencies: {
-            USD: {
-                symbol: "USD",
-                sign: "$",
-            },
-            EUR: {
-                symbol: "EUR",
-                sign: "€",
-            },
-            GBP: {
-                symbol: "GBP",
-                sign: "£",
-            },
-            CHF: {
-                symbol: "CHF",
-                sign: "Fr",
-            },
-            CAD: {
-                symbol: "CAD",
-                sign: "$",
-            },
-            AUD: {
-                symbol: "AUD",
-                sign: "$",
-            },
-            NZD: {
-                symbol: "NZD",
-                sign: "$",
-            },
-            JPY: {
-                symbol: "JPY",
-                sign: "¥",
-            },
+    },
+    fiatCurrencies: {
+        USD: {
+            symbol: "USD",
+            sign: "$",
         },
-        user: undefined,
-        account: undefined,
-        paymentMethods: undefined,
-        walletTotals: {balances: [], userCount: -1},
-        language: "en",
-        defaultFiatCurrencyCode: "USD",
-    }
+        EUR: {
+            symbol: "EUR",
+            sign: "€",
+        },
+        GBP: {
+            symbol: "GBP",
+            sign: "£",
+        },
+        CHF: {
+            symbol: "CHF",
+            sign: "Fr",
+        },
+        CAD: {
+            symbol: "CAD",
+            sign: "$",
+        },
+        AUD: {
+            symbol: "AUD",
+            sign: "$",
+        },
+        NZD: {
+            symbol: "NZD",
+            sign: "$",
+        },
+        JPY: {
+            symbol: "JPY",
+            sign: "¥",
+        },
+    },
+    user: undefined,
+    account: undefined,
+    paymentMethods: undefined,
+    walletTotals: {balances: [], userCount: -1},
+    language: "en",
+    defaultFiatCurrencyCode: "USD",
 };
