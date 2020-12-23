bcdb6b46d1adc95b0b52fc93fe3cc826be6e7615
Yaniv Mordekhay <yaniv.mordekhay@firstdag.com>
2020-12-23 16:20:03 +0200

    Improved tests separation into files and classes

--- vasp-validator/src/vasp_validator/tests/test_tx_no_travel_rule.py
+++ vasp-validator/src/vasp_validator/tests/test_tx_no_travel_rule.py
@@ -48,25 +48,3 @@ def test_receive_tx_no_travel_rule(validator, vasp_proxy: VaspProxy):
     assert vasp_proxy.knows_transaction(
         tx.onchain_version
     ), f"Transaction {tx.onchain_version} is not recognized by the VASP"
-
-
-def test_send_tx_with_travel_rule(validator, vasp_proxy: VaspProxy):
-    """
-    Successfully send a transaction requiring travel rule approval.
-    """
-    dest_address = validator.get_receiving_address()
-
-    tx = vasp_proxy.send_transaction(dest_address, 2_000_000_000, CURRENCY)
-
-    assert (
-        tx.status == TxStatus.COMPLETED
-    ), f"Failed to send transaction: {tx.status_description}"
-
-    # The amount is below $1000 so no off-chain interaction is required
-    # assert tx.offchain_refid is None
-
-    # VASP sent the transaction successfully. Validate that it was received
-    # by the validator
-    assert validator.knows_transaction(
-        tx.onchain_version
-    ), f"Transaction {tx.onchain_version} is not recognized by the validator"
