ca51c5eb8d0de3b8285b8467829da204588e0bcd
Yaniv Mordekhay <yaniv.mordekhay@firstdag.com>
2021-01-07 19:18:54 +0200

    FPPA command to model conversion as a separate function

--- backend/wallet/storage/funds_pull_pre_approval_commands.py
+++ backend/wallet/storage/funds_pull_pre_approval_commands.py
@@ -1,20 +1,14 @@
 from typing import List
 
-from .models import FundsPullPreApprovalCommands
-
-from . import db_session
+from . import db_session, models
 
 
-def commit_command(
-    command: FundsPullPreApprovalCommands,
-) -> FundsPullPreApprovalCommands:
+def commit_command(command: models.FundsPullPreApprovalCommands):
     db_session.add(command)
     db_session.commit()
 
-    return command
 
-
-def get_account_commands(account_id: int) -> List[FundsPullPreApprovalCommands]:
+def get_account_commands(account_id: int) -> List[models.FundsPullPreApprovalCommands]:
     # return [
     #     command
     #     for command in FundsPullPreApprovalCommands.query.filter_by(
@@ -24,24 +18,24 @@ def get_account_commands(account_id: int) -> List[FundsPullPreApprovalCommands]:
     return FundsPullPreApprovalCommands.query.filter_by(account_id=account_id).all()
 
 
-def get_command(funds_pre_approval_id: str) -> FundsPullPreApprovalCommands:
-    return FundsPullPreApprovalCommands.query.filter_by(
+def get_command(funds_pre_approval_id: str) -> models.FundsPullPreApprovalCommands:
+    return models.FundsPullPreApprovalCommands.query.filter_by(
         funds_pre_approval_id=funds_pre_approval_id
     ).first()
 
 
 def get_commands_by_type(
     account_id: int, type: str
-) -> List[FundsPullPreApprovalCommands]:
-    return FundsPullPreApprovalCommands.query.filter_by(
+) -> List[models.FundsPullPreApprovalCommands]:
+    return models.FundsPullPreApprovalCommands.query.filter_by(
         account_id=account_id, type=type
     ).all()
 
 
 def get_commands_by_status(
     account_id: int, status: str
-) -> List[FundsPullPreApprovalCommands]:
-    return FundsPullPreApprovalCommands.query.filter_by(
+) -> List[models.FundsPullPreApprovalCommands]:
+    return models.FundsPullPreApprovalCommands.query.filter_by(
         account_id=account_id, status=status
     ).all()
 
@@ -52,8 +46,8 @@ class FundsPullPreApprovalCommandNotFound(Exception):
 
 def update_command(
     funds_pre_approval_id: str, status: str
-) -> FundsPullPreApprovalCommands:
-    command = FundsPullPreApprovalCommands.query.get(funds_pre_approval_id)
+) -> models.FundsPullPreApprovalCommands:
+    command = models.FundsPullPreApprovalCommands.query.get(funds_pre_approval_id)
 
     if command:
         command.status = status
