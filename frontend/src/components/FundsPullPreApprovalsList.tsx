329c957d321f58a1d46d2308cb9bcb18a9e1423d
maya <maya.marton@firstdag.com>
2021-02-01 13:44:57 +0200

    temp

--- frontend/src/components/FundsPullPreApprovalsList.tsx
+++ frontend/src/components/FundsPullPreApprovalsList.tsx
@@ -32,61 +32,56 @@ function FundsPullPreApprovalsList({ approvals }: ApprovalsListProps) {
               key={approval.funds_pull_pre_approval_id}
               // onClick={() => onSelect && onSelect(transaction)}
             >
-              <div className="d-flex">
-                <>
-                  <span className="text-black mr-4 overflow-auto">
-                    <div>
-                      <span>
-                        Received from <b>{approval.biller_name}</b> (
-                        {new Date(approval.created_timestamp).toLocaleString()})
-                      </span>
-                    </div>
-
-                    <div className="text-black mr-4 overflow-auto">
-                      <strong>{"Limits:"}</strong>
-                      <div>
-                        {!approval.scope.max_cumulative_amount &&
-                          !approval.scope.max_transaction_amount &&
-                          "No Limits"}
-                      </div>
-                      <div>
-                        {approval.scope.max_transaction_amount &&
-                          "Single payment limit: Up to " +
-                            diemAmountToHumanFriendly(
-                              approval.scope.max_transaction_amount.amount
-                            )}{" "}
-                        {settings.currencies[approval.scope.max_transaction_amount.currency].sign}
-                      </div>
-                      <div>
-                        {approval.scope.max_cumulative_amount &&
-                          "Total payments limit: Up to " +
-                            diemAmountToHumanFriendly(
-                              approval.scope.max_cumulative_amount.max_amount.amount
-                            ) +
-                            " " +
-                            settings.currencies[
-                              approval.scope.max_cumulative_amount.max_amount.currency
-                            ].sign +
-                            " every " +
-                            approval.scope.max_cumulative_amount.value +
-                            " " +
-                            approval.scope.max_cumulative_amount.unit +
-                            (approval.scope.max_cumulative_amount.value > 1 ? "s" : "")}
-                      </div>
-                      <div className="small ml-auto ">
-                        {"Last payment allowed on "}
-                        {new Date(approval.scope.expiration_timestamp).toLocaleString()}
-                      </div>
-                    </div>
-                  </span>
-                  {/*<Button color="black" outline onClick={approveRequest}>*/}
-                  {/*  {"Approve"}*/}
-                  {/*</Button>*/}
-                  {/*<Button color="black" outline>*/}
-                  {/*  {"Reject"}*/}
-                  {/*</Button>*/}
-                </>
+              <div className="text-black mr-4 overflow-auto">
+                <span>
+                  Received from <b>{approval.biller_name}</b> (
+                  {new Date(approval.created_timestamp).toLocaleString()})
+                </span>
               </div>
+              <div className="text-black">
+                <strong>Limits:</strong>
+              </div>
+              <div className="text-black">
+                {!approval.scope.max_cumulative_amount &&
+                  !approval.scope.max_transaction_amount &&
+                  "No Limits"}
+              </div>
+              <div>
+                {approval.scope.max_transaction_amount &&
+                  "Single payment limit: Up to " +
+                    diemAmountToHumanFriendly(approval.scope.max_transaction_amount.amount)}{" "}
+                {settings.currencies[approval.scope.max_transaction_amount.currency].sign}
+              </div>
+              {/*        <div>*/}
+              {/*          {approval.scope.max_cumulative_amount &&*/}
+              {/*            "Total payments limit: Up to " +*/}
+              {/*              diemAmountToHumanFriendly(*/}
+              {/*                approval.scope.max_cumulative_amount.max_amount.amount*/}
+              {/*              ) +*/}
+              {/*              " " +*/}
+              {/*              settings.currencies[*/}
+              {/*                approval.scope.max_cumulative_amount.max_amount.currency*/}
+              {/*              ].sign +*/}
+              {/*              " every " +*/}
+              {/*              approval.scope.max_cumulative_amount.value +*/}
+              {/*              " " +*/}
+              {/*              approval.scope.max_cumulative_amount.unit +*/}
+              {/*              (approval.scope.max_cumulative_amount.value > 1 ? "s" : "")}*/}
+              {/*        </div>*/}
+              {/*        <div className="small ml-auto ">*/}
+              {/*          {"Last payment allowed on "}*/}
+              {/*          {new Date(approval.scope.expiration_timestamp).toLocaleString()}*/}
+              {/*        </div>*/}
+              {/*      </div>*/}
+              {/*    </span>*/}
+              {/*    /!*<Button color="black" outline onClick={approveRequest}>*!/*/}
+              {/*    /!*  {"Approve"}*!/*/}
+              {/*    /!*</Button>*!/*/}
+              {/*    /!*<Button color="black" outline>*!/*/}
+              {/*    /!*  {"Reject"}*!/*/}
+              {/*    /!*</Button>*!/*/}
+              {/*  </>*/}
+              {/*</div>*/}
             </li>
           );
         })}
