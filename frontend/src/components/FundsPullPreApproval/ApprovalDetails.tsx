5c1be03f6d52537a7938d030c0b67ea70e3d0b0e
maya <maya.marton@firstdag.com>
2021-02-07 12:44:02 +0200

    display dedicated date text to each status

--- frontend/src/components/FundsPullPreApproval/ApprovalDetails.tsx
+++ frontend/src/components/FundsPullPreApproval/ApprovalDetails.tsx
@@ -10,13 +10,41 @@ interface RequestDetailsProps {
 function ApprovalDetails({ approval }: RequestDetailsProps) {
   const [settings] = useContext(settingsContext)!;
 
+  function dateToDisplay(text: string, timestamp: string) {
+    return (
+      <>
+        {text} <span>{new Date(timestamp).toLocaleString()}</span>
+      </>
+    );
+  }
+
+  const dateToDisplayByApprovalStatus = () => {
+    if (approval!.status === "pending") {
+      return dateToDisplay("on", approval!.created_at);
+    }
+    if (approval!.status === "rejected") {
+      return dateToDisplay("rejected on", approval!.updated_at);
+    }
+    if (approval!.status === "closed") {
+      return dateToDisplay("revoked on", approval!.updated_at);
+    }
+    if (approval!.status === "valid") {
+      const now = new Date().toISOString();
+      if (Date.parse(approval!.scope.expiration_timestamp) < Date.parse(now)) {
+        return dateToDisplay("expired at", approval!.scope.expiration_timestamp);
+      }
+
+      return dateToDisplay("approved on", approval!.updated_at);
+    }
+  };
+
   return (
     <span>
       <div className="text-black">
         <span>
           Received from <b>{approval!.biller_name}</b>
         </span>{" "}
-        {"on"} <span>{new Date(approval!.created_timestamp).toLocaleString()}</span>
+        {dateToDisplayByApprovalStatus()}
       </div>
       {approval?.description && <div>{approval.description}</div>}
       <div className="pt-1">
