7cc7c0467af90719e0af530fe00295b361ef20df
maya <maya.marton@firstdag.com>
2021-02-09 11:14:28 +0200

    add indication for fail\success BE requests in Modals

--- frontend/src/components/FundsPullPreApproval/RejectModal.tsx
+++ frontend/src/components/FundsPullPreApproval/RejectModal.tsx
@@ -1,8 +1,11 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Button, Modal, ModalBody } from "reactstrap";
 import CloseButton from "../CloseButton";
 import { Approval } from "../../interfaces/approval";
 import BackendClient from "../../services/backendClient";
+import { BackendError } from "../../services/errors";
+import ErrorMessage from "../Messages/ErrorMessage";
+import SuccessMessage from "../Messages/SuccessMessage";
 
 interface RejectModalProps {
   approval: Approval;
@@ -11,37 +14,80 @@ interface RejectModalProps {
 }
 
 function RejectModal({ approval, open, onClose }: RejectModalProps) {
+  const [submitStatus, setSubmitStatus] = useState<"edit" | "fail" | "success">("edit");
+  const [errorMessage, setErrorMessage] = useState<string>();
+
+  useEffect(() => {
+    async function refreshUser() {
+      try {
+        await new BackendClient().refreshUser();
+      } catch (e) {
+        console.error(e);
+      }
+    }
+    // noinspection JSIgnoredPromiseFromCall
+    refreshUser();
+  }, []);
+
   const updateApproval = async () => {
     try {
-      await new BackendClient().updateApprovalStatus(
-        approval.funds_pull_pre_approval_id,
-        "rejected"
-      );
-    } catch (e) {
-      console.error(e);
-    }
+      setErrorMessage(undefined);
+      setSubmitStatus("edit");
 
-    onClose();
+      const backendClient = new BackendClient();
+      await backendClient.updateApprovalStatus(approval.funds_pull_pre_approval_id, "rejected");
+
+      setSubmitStatus("success");
+      await backendClient.refreshUser();
+    } catch (e) {
+      setSubmitStatus("fail");
+      if (e instanceof BackendError) {
+        setErrorMessage(e.message);
+      } else {
+        setErrorMessage("Internal Error");
+        console.error("Unexpected error", e);
+      }
+    }
   };
+
+  function onModalClose() {
+    setSubmitStatus("edit");
+    setErrorMessage(undefined);
+    onClose();
+  }
 
   return (
     <Modal className="modal-dialog-centered" isOpen={open} onClosed={onClose}>
       <ModalBody>
-        <CloseButton onClick={onClose} />
-        <h3>Reject Request</h3>
-        <div className="text-black  pb-5">
-          Are you sure you want to reject the request from {approval?.biller_name}?
-        </div>
-        <span>
-          <div className="float-right">
-            <Button color="black" outline onClick={onClose} className="mr-1">
-              Cancel
+        <CloseButton onClick={onModalClose} />
+        {errorMessage && <ErrorMessage message={errorMessage} />}
+        {submitStatus === "success" && (
+          <>
+            <SuccessMessage message={"Approval status successfully updated"} />
+            <Button onClick={onModalClose} color="black" outline className="mt-2">
+              Done
             </Button>
-            <Button color="black" onClick={updateApproval}>
-              Reject
-            </Button>
-          </div>
-        </span>
+          </>
+        )}
+
+        {submitStatus !== "success" && (
+          <>
+            <h3>Reject Request</h3>
+            <div className="text-black  pb-5">
+              Are you sure you want to reject the request from {approval?.biller_name}?
+            </div>
+            <span>
+              <div className="float-right">
+                <Button color="black" outline onClick={onModalClose} className="mr-1">
+                  Cancel
+                </Button>
+                <Button color="black" onClick={updateApproval}>
+                  Reject
+                </Button>
+              </div>
+            </span>
+          </>
+        )}
       </ModalBody>
     </Modal>
   );
--- frontend/src/components/FundsPullPreApproval/RevokeModal.tsx
+++ frontend/src/components/FundsPullPreApproval/RevokeModal.tsx
@@ -1,8 +1,11 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Button, Modal, ModalBody } from "reactstrap";
 import CloseButton from "../CloseButton";
 import { Approval } from "../../interfaces/approval";
 import BackendClient from "../../services/backendClient";
+import { BackendError } from "../../services/errors";
+import ErrorMessage from "../Messages/ErrorMessage";
+import SuccessMessage from "../Messages/SuccessMessage";
 
 interface RevokeModalProps {
   approval: Approval;
@@ -11,34 +14,80 @@ interface RevokeModalProps {
 }
 
 function RevokeModal({ approval, open, onClose }: RevokeModalProps) {
+  const [submitStatus, setSubmitStatus] = useState<"edit" | "fail" | "success">("edit");
+  const [errorMessage, setErrorMessage] = useState<string>();
+
+  useEffect(() => {
+    async function refreshUser() {
+      try {
+        await new BackendClient().refreshUser();
+      } catch (e) {
+        console.error(e);
+      }
+    }
+    // noinspection JSIgnoredPromiseFromCall
+    refreshUser();
+  }, []);
+
   const updateApproval = async () => {
     try {
-      await new BackendClient().updateApprovalStatus(approval.funds_pull_pre_approval_id, "closed");
-    } catch (e) {
-      console.error(e);
-    }
+      setErrorMessage(undefined);
+      setSubmitStatus("edit");
 
-    onClose();
+      const backendClient = new BackendClient();
+      await backendClient.updateApprovalStatus(approval.funds_pull_pre_approval_id, "closed");
+
+      setSubmitStatus("success");
+      await backendClient.refreshUser();
+    } catch (e) {
+      setSubmitStatus("fail");
+      if (e instanceof BackendError) {
+        setErrorMessage(e.message);
+      } else {
+        setErrorMessage("Internal Error");
+        console.error("Unexpected error", e);
+      }
+    }
   };
+
+  function onModalClose() {
+    setSubmitStatus("edit");
+    setErrorMessage(undefined);
+    onClose();
+  }
 
   return (
     <Modal className="modal-dialog-centered" isOpen={open} onClosed={onClose}>
       <ModalBody>
-        <CloseButton onClick={onClose} />
-        <h3>Revoke Request</h3>
-        <div className="text-black pb-5">
-          Are you sure you want to revoke the request from {approval?.biller_name}?
-        </div>
-        <span>
-          <div className="float-right">
-            <Button onClick={onClose} color="black" outline className="mr-1">
-              Cancel
+        <CloseButton onClick={onModalClose} />
+        {errorMessage && <ErrorMessage message={errorMessage} />}
+        {submitStatus === "success" && (
+          <>
+            <SuccessMessage message={"Approval status successfully updated"} />
+            <Button onClick={onModalClose} color="black" outline className="mt-2">
+              Done
             </Button>
-            <Button onClick={updateApproval} color="black">
-              Revoke
-            </Button>
-          </div>
-        </span>
+          </>
+        )}
+
+        {submitStatus !== "success" && (
+          <>
+            <h3>Revoke Request</h3>
+            <div className="text-black pb-5">
+              Are you sure you want to revoke the request from {approval?.biller_name}?
+            </div>
+            <span>
+              <div className="float-right">
+                <Button onClick={onModalClose} color="black" outline className="mr-1">
+                  Cancel
+                </Button>
+                <Button onClick={updateApproval} color="black">
+                  Revoke
+                </Button>
+              </div>
+            </span>{" "}
+          </>
+        )}
       </ModalBody>
     </Modal>
   );
