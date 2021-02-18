bc07ca682cff1d6030c22b1520ee160068b2807e
maya <maya.marton@firstdag.com>
2021-02-18 11:09:17 +0200

    increase INVENTORY_AMOUNT to 100_000_000_000

--- backend/wallet/services/inventory.py
+++ backend/wallet/services/inventory.py
@@ -29,7 +29,7 @@ from wallet.types import (
 logger = logging.getLogger(__name__)
 
 INVENTORY_COVER_CURRENCY = Currency.USD
-INVENTORY_AMOUNT = 950_000_000
+INVENTORY_AMOUNT = 100_000_000_000
 
 
 def wait_for_trade_to_complete(trade_id):
