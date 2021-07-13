086070f318c959b8fdda89dfb6a06c7910780d72
maya <maya.marton@firstdag.com>
2021-07-13 12:01:50 +0300

    change\improve components names

--- backend/wallet/services/offchain/p2m_payment.py
+++ backend/wallet/services/offchain/p2m_payment.py
@@ -65,7 +65,9 @@ def get_payment_details(
                 request_sender_address=my_address,
                 counterparty_account_id=vasp_address,
                 request_bytes=jws.serialize(
-                    new_get_info_request(reference_id=reference_id, cid=reference_id),
+                    new_get_payment_info_request(
+                        reference_id=reference_id, cid=reference_id
+                    ),
                     utils.compliance_private_key().sign,
                 ),
             )
